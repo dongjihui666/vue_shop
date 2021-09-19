01c6d3e4428b1dca215a88bc0d1f022ddec9fb94
dongjihui666 <dongjihui0320@gmail.com>
2021-09-19 15:56:04 +0900

    用户列表功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 
-import {Button,Form,FormItem,input,Message} from 'element'
+import {Button,Form,FormItem,Input,Message,Header,Container,Aside,Main,Menu,Submenu,MenuItemGroup,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,Dialog,Pagination} from 'element-ui'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
-Vue.use(input)
+Vue.use(Input)
+Vue.use(Header)
+Vue.use(Container)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Dialog)
+Vue.use(Pagination)
 Vue.prototype.$message = Message
